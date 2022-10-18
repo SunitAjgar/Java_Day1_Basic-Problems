af7700331b81c535e5b19ef7ed5960eb07e539a6
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-10-19 04:35:49 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 public class LeapYear {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter an Year :: ");
+		System.out.println("Enter the year for checking Leap year ");
 		int year = sc.nextInt();
 		if (year >= 1582) {
 			if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
-				System.out.println("Entered year is a leap year");
+				System.out.println("The entered year is a leap year");
 			else
-				System.out.println("Entered year is not a leap year");
+				System.out.println("The entered year is not a leap year");
 		}
 		else {
 			System.out.println("Please Enter a year after 1582");
