c797e4a1713b3161be70b65787cc4588865814ef
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-10-19 04:31:12 +0530

    Checking Leap Year

---
+++ LeapYear.java
@@ -0,0 +1,21 @@
+//Q.1.7 Write a program to check leap year
+package com.bridgelabz.practice;
+
+import java.util.Scanner;
+
+public class LeapYear {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter an Year :: ");
+		int year = sc.nextInt();
+		if (year >= 1582) {
+			if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+				System.out.println("Entered year is a leap year");
+			else
+				System.out.println("Entered year is not a leap year");
+		}
+		else {
+			System.out.println("Please Enter a year after 1582");
+		}
+	}
+}
