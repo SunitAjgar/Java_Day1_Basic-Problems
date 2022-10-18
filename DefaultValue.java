cce58b716113ff53cd3b6763080b24b740b6d518
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-10-18 19:26:38 +0530

    Displaying Default Value of data types

---
+++ DefaultValue.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.practice;
+
+//Q-1.2 Write a Java program to display the default value of all primitive data types of Java
+
+public class DefaultValue {
+
+	static boolean value;
+	static byte a;
+	static char ch;
+	static short sh;
+	static int num;
+	static float fl;
+	static double dl;
+	static long var;
+
+	public static void main(String[] args) {
+
+		System.out.println("The default value for the field boolean dataype is : " + value);
+		System.out.println("The default value for the field byte dataype is : " + a);
+		System.out.println("The default value for the field char dataype is : " + ch);
+		System.out.println("The default value for the field short dataype is : " + sh);
+		System.out.println("The default value for the field int dataype is : " + num);
+		System.out.println("The default value for the field float dataype is : " + fl);
+		System.out.println("The default value for the field double dataype is : " + dl);
+		System.out.println("The default value for the field long dataype is : " + var);
+
+	}
+}
