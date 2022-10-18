6db1e74a9f49a2476ff443421e04f0992ebf519b
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-10-19 03:40:44 +0530

    Displaying message from Command Line

---
+++ Message.java
@@ -0,0 +1,9 @@
+//Q. 1.5 To Display the name using the command line.
+
+package com.bridgelabz.practice;
+
+public class Message {
+	public static void main(String[] args) {
+     System.out.println("Hello bridgelabz" + args[0]);
+	}
+}
